fb28fb40ccc07972d619386b711756c9ca774e15
Gyanshu Kumar <119155250+Gyanshu-Kumar@users.noreply.github.com>
2023-10-01 15:25:43 +0530

    Create SearchElement.cpp
    Added code Search Element In Array

---
+++ 04. Arrays/SearchElement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[] = {3, 4, 5, 6, 7};
+    int size = sizeof(array) / sizeof(array[0]);
+    int key = 4;
+
+    int ans = -1;
+    for (int i = 0; i < size; i++) {
+        if (array[i] == key) {
+            ans = i;
+            break;  
+        }
+    }
+
+    cout << ans << endl;  
+    return 0;
+}
+
